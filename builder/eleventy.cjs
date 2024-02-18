26637f3de8e2e2760b94f0bf5f47f0b876ecffa1
Andy Preston <edgeeffect@gmail.com>
2024-02-18 21:13:27 +0000

    add some explanations to eleventy.cjs

--- builder/eleventy.cjs
+++ builder/eleventy.cjs
@@ -6,6 +6,7 @@ const webCPlugin = require("@11ty/eleventy-plugin-webc");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addGlobalData("permalink", () => {
+        // ensure page urls are `page.html` not `/page/'
         return (data) =>
             `${data.page.filePathStem}.${data.page.outputFileExtension}`;
     });
@@ -18,6 +19,7 @@ module.exports = function (eleventyConfig) {
         ],
         "bundlePluginOptions": {
             "toFileDirectory": "",
+            // Run the transformers on the CSS and JS
             "transforms": [
                 function (content) {
                     return transforms(content, this.type);
@@ -27,7 +29,7 @@ module.exports = function (eleventyConfig) {
     });
     eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.on("eleventy.config", async (params) => {
-        /* This is something of a horrible hack to ensure that tidying
+        /* This is something of a horrible hack to ensure that page tidying
            transformers are run AFTER the transformer from
            `eleventy-plugin-bundle` */
         params.config.transforms["andy-preston-transforms"] = transforms;
