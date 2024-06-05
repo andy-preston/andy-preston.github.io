43f5de439be11690cd80b9bfc2cb36bf343153a6
Andy Preston <edgeeffect@gmail.com>
2024-06-05 18:28:50 +0100

    Deleting some bits of the old site generator that are no longer relevant

--- builder/eleventy.js
+++ builder/eleventy.js
@@ -1,24 +1,9 @@
 "use strict";
 
-// cSpell:words syntaxhighlight
-
-import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import transforms from "./transforms.ts";
-import webCPlugin from "@11ty/eleventy-plugin-webc";
 
 export default async function(eleventyConfig) {
-    eleventyConfig.addGlobalData("permalink", () => {
-        // ensure page urls are `page.html` not `/page/'
-        return (data) =>
-            `${data.page.filePathStem}.${data.page.outputFileExtension}`;
-    });
-    eleventyConfig.addGlobalData("layout", "layout.webc");
-    eleventyConfig.addPassthroughCopy({ "fixed/*": "." });
     eleventyConfig.addPlugin(webCPlugin, {
-        "components": [
-            "components/*.webc",
-            "npm:@11ty/eleventy-plugin-syntaxhighlight/*.webc"
-        ],
         "bundlePluginOptions": {
             "toFileDirectory": "",
             // Run the transformers on the CSS and JS
@@ -29,20 +14,10 @@ export default async function(eleventyConfig) {
             ]
         }
     });
-    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.on("eleventy.config", async (params) => {
         /* This is something of a horrible hack to ensure that page tidying
            transformers are run AFTER the transformer from
            `eleventy-plugin-bundle` */
         params.config.transforms["andy-preston-transforms"] = transforms;
     });
-    return {
-        "dir": {
-            "input": "views",
-            "output": "site",
-            "includes": "../includes",
-            "layouts": "../layout"
-        },
-        "templateFormats": ["webc"]
-    };
 }
