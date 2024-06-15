ea35d7ecc8e39f8402533407f17dd4510af4dd0d
Andy Preston <edgeeffect@gmail.com>
2024-06-15 21:16:27 +0100

    basic support for postcss

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import markdownItAttrs from "npm:markdown-it-attrs";
 import code_highlight from "lume/plugins/code_highlight.ts";
+import postcss from "lume/plugins/postcss.ts";
 import typescriptProcessor from "./_includes/typescript.ts";
 import article from "./_includes/article/article.ts";
 import index from "./_includes/index.ts";
@@ -22,8 +23,9 @@ siteBuilder
     // "builder" directory should be removed eventually
     .ignore("README.md", "fixed", "builder")
     .use(code_highlight())
-    .loadAssets([".css", ".ts"])
+    .use(postcss({}))
     .preprocess([".html"], index)
+    .loadAssets([".ts"])
     .process([".ts"], typescriptProcessor)
     .process([".html"], htmlTransform)
     .process([".html"], htmlFormat)
