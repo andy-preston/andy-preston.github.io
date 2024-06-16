b87207edc5abed52f0de40dd9bd0d11f880757e6
Andy Preston <edgeeffect@gmail.com>
2024-06-16 22:20:51 +0100

    post CSS and style sheets

--- _includes/postCss.ts
+++ _includes/postCss.ts
@@ -1,12 +1,20 @@
-import postcssCustomProperties from "npm:postcss-custom-properties";
-import postcssRemoveRoot from "npm:postcss-remove-root";
+import combineDuplicatedSelectors from "npm:postcss-combine-duplicated-selectors";
+import combineMediaQuery from "npm:postcss-combine-media-query";
+import customMedia from "npm:postcss-custom-media";
+import customProperties from "npm:postcss-custom-properties";
+import discardComments from "npm:postcss-discard-comments";
+import removeRoot from "npm:postcss-remove-root";
+import perfectionist from "npm:perfectionist";
 import postcss from "lume/plugins/postcss.ts";
 
 export default postcss({
     "plugins": [
-        postcssCustomProperties({
-            "preserve": false
-        }),
-        postcssRemoveRoot()
+        customProperties({ "preserve": false }),
+        customMedia(),
+        combineMediaQuery(),
+        combineDuplicatedSelectors(),
+        removeRoot(),
+        discardComments(),
+        perfectionist({ "format": "compact" })
     ],
 });
