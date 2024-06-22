708327b2e7657af3a7bdea2a50b805e1d34c387b
Andy Preston <edgeeffect@gmail.com>
2024-06-22 22:39:40 +0100

    We need loads less plugins for post-css now

--- _deps/postcss.ts
+++ _deps/postcss.ts
@@ -3,24 +3,8 @@ export {
 } from "npm:postcss-combine-duplicated-selectors";
 
 export {
-    default as combineMediaQuery
-} from "npm:postcss-combine-media-query";
-
-export {
-    default as customMedia
-} from "npm:postcss-custom-media";
-
-export {
-    default as customProperties
-} from "npm:postcss-custom-properties";
-
-export {
     default as discardComments
 } from "npm:postcss-discard-comments";
-
-export {
-    default as removeRoot
-} from "npm:postcss-remove-root";
 
 export {
     default as perfectionist
--- style/_postCss.ts
+++ style/_postCss.ts
@@ -1,21 +1,13 @@
 import {
     combineDuplicatedSelectors,
-    combineMediaQuery,
-    customMedia,
-    customProperties,
     discardComments,
-    removeRoot,
     perfectionist
 } from "../_deps/postcss.ts";
 import postcss from "lume/plugins/postcss.ts";
 
 export default postcss({
     "plugins": [
-        customProperties({ "preserve": false }),
-        customMedia(),
-        combineMediaQuery(),
         combineDuplicatedSelectors(),
-        removeRoot(),
         discardComments(),
         perfectionist({ "format": "compact" })
     ],
