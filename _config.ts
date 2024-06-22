3e43ff002887bc65810ee7ec844569117185e383
Andy Preston <edgeeffect@gmail.com>
2024-06-22 20:07:37 +0100

    finalising deps and old _include

--- _config.ts
+++ _config.ts
@@ -2,11 +2,12 @@ import lume from "lume/mod.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import { markdownItAttrs } from "./_deps/lume.ts";
 import frontPageProcess from "./front-page/_process.ts";
-import postCss from "./style/_postCss.ts";
 import { esBuildPlugin, javaScriptUrl } from "./front-page/illustration/_build.ts";
 import articleBuilder from "./_articles/markdown/markdownItPlugin.ts";
 import articlesDom from "./_articles/dom/mod.ts";
 import domFormat from "./_all_pages/_domFormat/mod.ts";
+import postCss from "./style/_postCss.ts";
+
 const markdown = {
     "html": false,
     "plugins": [
