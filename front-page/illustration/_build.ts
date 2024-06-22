a7094a62f5e59e61d1dd6e1dda5b725293b2d50d
Andy Preston <edgeeffect@gmail.com>
2024-06-22 16:27:39 +0100

    Got the site JS down to ES5 by hook or by crook

--- front-page/illustration/_build.ts
+++ front-page/illustration/_build.ts
@@ -1,24 +1,52 @@
 import esBuild from "lume/plugins/esbuild.ts";
+import { typescript } from "../../_includes/deps.ts";
+import {
+    OnLoadArgs,
+    OnLoadResult,
+    Plugin,
+    PluginBuild
+} from "lume/deps/esbuild.ts";
+import options from "./tsconfig.json" with { type: "json" };
 
-export const esBuildPlugin = esBuild(
-    {
-        "options": {
-            "format": "iife",
-            "minify": false,
-            "minifyWhitespace": true,
-            "keepNames": false,
-        }
+const transpile = (args: OnLoadArgs): OnLoadResult => {
+    const path = args.path.replace(/^file:\/\//, "");
+    const source = Deno.readTextFileSync(path);
+    return {
+        "contents": typescript.transpileModule(
+            source,
+            // It's horrible doing `as unknown as SomethingElse`
+            // but this gets the job done for now.
+            options as unknown as typescript.TranspileOptions
+        ).outputText,
+        "loader": "js"
+    };
+}
+
+const typescriptPlugin: Plugin = {
+    "name": "typescriptPlugin",
+    setup(build: PluginBuild) {
+        build.onLoad({ "filter": /\.ts$/ }, transpile);
     }
-);
-
-const urls = {
-    "illustration": "/front-illustration.js"
 };
+
+export const esBuildPlugin = esBuild({
+    "options": {
+        "plugins": [typescriptPlugin],
+        // cSpell:words iife
+        "format": "iife",
+        "minify": false,
+        "minifyWhitespace": true,
+        "keepNames": false
+    }
+});
 
 export const javaScriptUrl = (
     filteredPages: Array<Lume.Page>,
     _allPages: Array<Lume.Page>
 ) => {
+    const urls = {
+        "illustration": "/front-illustration.js"
+    };
     filteredPages.forEach(
         (page: Lume.Page) => {
             page.data.url = urls[page.data.basename as keyof typeof urls];
