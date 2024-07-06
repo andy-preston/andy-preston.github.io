5e3f6cf61492db4432971937b66451af193cb5ac
Andy Preston <edgeeffect@gmail.com>
2024-07-06 13:34:06 +0100

    typeof is not a function

--- _all-pages/testDom.ts
+++ _all-pages/testDom.ts
@@ -1,7 +1,7 @@
 import { DOMParser } from "lume/deps/dom.ts";
 
 export const documentFromHtml = (html: string | Array<string>): Document => {
-    const htmlString = typeof(html) == "string" ? html : html.join("\n");
+    const htmlString = typeof html == "string" ? html : html.join("\n");
     return new DOMParser().parseFromString(
         htmlString,
         "text/html"
