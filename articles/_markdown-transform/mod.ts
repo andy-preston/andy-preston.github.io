9d8a4504e7c409b44365663816c2c7d2525a18b6
Andy Preston <edgeeffect@gmail.com>
2024-07-06 13:39:14 +0100

    It looks complex but it's just reformatting params on a function

--- articles/_markdown-transform/mod.ts
+++ articles/_markdown-transform/mod.ts
@@ -5,23 +5,21 @@ import type { MarkdownItState } from "./markdownItTypes.ts";
 import { tokenTransform } from "./tokenTransform.ts";
 
 export const markdownTransform = (markdownIt: MarkdownIt) => {
-    markdownIt.core.ruler.push(
-        "articlePrep",
-        (state: MarkdownItState) => {
-            const pageData = state.env.data!.page!.data!;
-            if (pageData.basename != "front-page") {
-                const transform = tokenTransform(state.tokens, pageData.basename);
-                state.tokens = transform.tokens;
+    markdownIt.core.ruler.push("articlePrep", (state: MarkdownItState) => {
+        const pageData = state.env.data!.page!.data!;
+        if (pageData.basename != "front-page") {
+            const transform = tokenTransform(state.tokens, pageData.basename);
+            state.tokens = transform.tokens;
 
-                pageData.title = transform.title;
-                pageData.htmlTitle = htmlTitle(pageData.title);
+            pageData.title = transform.title;
+            pageData.htmlTitle = htmlTitle(pageData.title);
 
-                [pageData.humanDate, pageData.shortDate] =
-                    displayDates(pageData.noDate ? "" : pageData.date);
+            [pageData.humanDate, pageData.shortDate] = displayDates(
+                pageData.noDate ? "" : pageData.date
+            );
 
-                pageData.layout = "../_layout.vto";
-                pageData.url = `/${pageData.basename}/`;
-            }
+            pageData.layout = "../_layout.vto";
+            pageData.url = `/${pageData.basename}/`;
         }
-    );
+    });
 };
