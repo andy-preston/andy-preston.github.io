0dd759a23f2f70579a7f8533719530877c2a0468
Andy Preston <edgeeffect@gmail.com>
2024-07-18 12:20:53 +0100

    better hljsCoverage.test

--- _all-pages/hljsCoverage.test.ts
+++ _all-pages/hljsCoverage.test.ts
@@ -6,7 +6,6 @@ const hljsClassesIn = (path: string): IterableIterator<RegExpExecArray> =>
 
 Deno.test("hljs styles that are used are covered in CSS", () => {
     let styles: Array<string> = [];
-
     for (const path of siteFiles("css")) {
         styles = styles.concat(
             Array.from(hljsClassesIn(path)).map(
@@ -14,10 +13,27 @@ Deno.test("hljs styles that are used are covered in CSS", () => {
             )
         );
     }
-
+    styles = [...new Set(styles)];
     for (const path of siteFiles("html")) {
         for (const usedClass of hljsClassesIn(path)) {
             assertArrayIncludes(styles, [usedClass[0]]);
+        }
+    }
+});
+
+Deno.test("hljs styles that are in CSS are used", () => {
+    let usedClasses: Array<string> = [];
+    for (const path of siteFiles("html")) {
+        usedClasses = usedClasses.concat(
+            Array.from(hljsClassesIn(path)).map(
+                (match: RegExpExecArray): string => match[0]
+            )
+        );
+    }
+    usedClasses = [...new Set(usedClasses)];
+    for (const path of siteFiles("css")) {
+        for (const style of hljsClassesIn(path)) {
+            assertArrayIncludes(usedClasses, [style[0]]);
         }
     }
 });
